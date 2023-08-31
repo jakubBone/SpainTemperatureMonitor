95313c2ab5567a5dd8a4c80576b110d434f29922
jakubBone <jakub.bone@poczta.onet.pl>
2023-08-31 13:52:57 +0200

    Commit 5: temperaturSorter method replaced to  the Month class

--- src/Month.java
+++ src/Month.java
@@ -1,6 +1,7 @@
 public class Month {
     int days;
-    int [] temperatureList;
+    int[] temperatureList;
+
     public Month(int days) {
         this.days = days;
     }
@@ -8,15 +9,30 @@ public class Month {
     // Daily temperature creator method illustrating every day monitoring
     // Temperature is set randomly from the range of 25 - 45 degrees
     // Temperature order -> from 1st to the last day of month
-    public void createTemperature(){
+    public void createTemperature() {
         temperatureList = new int[days];
-        for(int i = 0; i < days; i++){
+        for (int i = 0; i < days; i++) {
             temperatureList[i] = (int) (Math.random() * (45 - 25) + 25);
         }
     }
+
     public void printTemperature() {
         for (int element : temperatureList) {
             System.out.print(element + ", ");
         }
+    }
+
+    public void temperatureSorter() {
+        int parameter = 0;
+        for (int i = 0; i < temperatureList.length - 1; i++) {
+            for (int j = 0; j < temperatureList.length - 1; j++) {
+                if (temperatureList[j] > temperatureList[j + 1]) {
+                    parameter = temperatureList[j + 1];
+                    temperatureList[j + 1] = temperatureList[j];
+                    temperatureList[j] = parameter;
+                }
+            }
+        }
+
     }
 }
--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -1,29 +1,14 @@
 public class TemperatureTest {
     public static void main(String[] args) {
-
-        TemperatureTest x = new TemperatureTest();
         Month august = new Month(31);
         august.createTemperature();
         august.printTemperature();
+
         System.out.println();
 
-
-        x.temperatureSorter(august.temperatureList);
+        august.temperatureSorter();
         august.printTemperature();
 
-
-    }
-    public void temperatureSorter(int[] temperatureList) {
-        int parameter = 0;
-        for (int i = 0; i < temperatureList.length - 1 ; i++) {
-            for (int j = 0; j < temperatureList.length - 1; j++) {
-                if (temperatureList[j] > temperatureList[j + 1]) {
-                    parameter = temperatureList[j + 1];
-                    temperatureList[j + 1] = temperatureList[j];
-                    temperatureList[j] = parameter;
-                }
-            }
-        }
     }
 }
 
