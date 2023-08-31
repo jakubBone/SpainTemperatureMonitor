b720c5f7915582cad7511c739f1d0a8901506678
jakubBone <jakub.bone@poczta.onet.pl>
2023-08-31 12:46:07 +0200

    Commit 2: Improve the temperatureDaysComparator sorting method and add example table in order to check the logic

--- src/SpainTemperatureMonitor.java
+++ src/SpainTemperatureMonitor.java
@@ -9,17 +9,28 @@ public class SpainTemperatureMonitor {
                 25, 26, 27, 28, 29, 30, 31, 31, 29, 28, 27, 26, 25, 24, 23};
         int[] septemberSevillaTemperatures = {35, 36, 36, 33, 32, 32, 34, 35, 34, 33, 31, 30, 29, 28, 27, 29,
                 30, 31, 32, 33, 34, 35, 35, 33, 32, 31, 30, 29, 28, 27};
+
         int[] tab = {3, 4, 5, 1, 2, 4, 5};
+        for (int element: tab){
+            System.out.print("element + " ");
+        }
+        System.out.println();
+
+        x.temperatureDaysComparator(tab);
+        for (int element: tab){
+            System.out.print(element + ", ");
+        }
 
     }
     public void temperatureDaysComparator(int[] temperatureList) {
-        int parameter =0;
-        for (int i = 0; i < temperatureList.length -1 ; i++) {
+        int parameter = 0;
+        for (int i = 0; i < temperatureList.length - 1 ; i++) {
             for (int j = 0; j < temperatureList.length - 1; j++) {
-                if (temperatureList[j] > temperatureList[j + 1])
+                if (temperatureList[j] > temperatureList[j + 1]) {
                     parameter = temperatureList[j + 1];
                     temperatureList[j + 1] = temperatureList[j];
                     temperatureList[j] = parameter;
+                }
             }
         }
     }
