dfd3222b24890a6fe6fa40740cc74a97cd26d935
jakubBone <jakub.bone@poczta.onet.pl>
2023-09-01 10:08:23 +0200

    Commit 7: Added a new askForMonth method asking the user for month and year data. City class making in prograss

--- src/Month.java
+++ src/Month.java
@@ -1,29 +1,50 @@
-public abstract class Month {
+import java.util.Scanner;
 
-    String monthName;
+public abstract class Month {
+    int monthIndex;
+    int year;
     int monthDays;
     int[] temperatureList;
 
-    //public Month(int days) {
-        //this.days = days;
-    //}
+    public void askForMonth(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose the year: ");
+        year = sc.nextInt();
+
+        System.out.println("Choose the month: (January - 1, February - 2, ..., December - 12: ");
+        monthIndex = sc.nextInt();
+
+        switch (monthIndex){
+            case 1, 3, 5, 7, 8, 10, 12:
+                monthDays = 31;
+                break;
+            case 4, 6, 9, 11:
+                monthDays = 30;
+                break;
+            case 2:
+                if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                    monthDays = 29;
+                else
+                    monthDays = 28;
+        }
+    }
+
 
     // Daily temperature creator method illustrating every day monitoring
     // Temperature is set randomly from the range of 25 - 45 degrees
     // Temperature order -> from 1st to the last day of month
     public void createTemperature() {
         temperatureList = new int[monthDays];
+        if(monthIndex >= 0 && monthIndex <= 4)
         for (int i = 0; i < monthDays; i++) {
             temperatureList[i] = (int) (Math.random() * (45 - 25) + 25);
         }
     }
-
     public void printTemperature() {
         for (int element : temperatureList) {
             System.out.print(element + ", ");
         }
     }
-
     public void temperatureSorter() {
         int parameter = 0;
         for (int i = 0; i < temperatureList.length - 1; i++) {
--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class TemperatureTest {
     public static void main(String[] args) {
-
-
-
-        City barcelona = new City();
-        barcelona.monthName = "September";
-        barcelona.monthDays = 31;
+        
         barcelona.createTemperature();
         barcelona.printTemperature();
 
         barcelona.temperatureSorter();
         barcelona.printTemperature();
+
+        barcelona.askForMonth();
 
     }
 }
