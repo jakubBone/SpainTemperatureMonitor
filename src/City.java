010d9d81e1ad2aae40293a36b8ce18c8c8071852
jakubBone <jakub.bone@poczta.onet.pl>
2023-09-04 12:43:54 +0200

    Commit 16: Refactored strings in sout methods

--- src/City.java
+++ src/City.java
@@ -12,13 +12,13 @@ public class City extends Month{
         askForDate();
         createTemperature();
         System.out.println();
-        System.out.print(name + " - temperatures from 1st to last day of month ||  ");
+        System.out.print(name + " - temperatures from 1st to last day of month:  ");
         printTemperature();
         temperatureSorter();
-        System.out.print(name + " - temperatures ordered from the the smallest to the highest || ");
+        System.out.print(name + " - temperatures ordered from the the smallest to the highest: ");
         printTemperature();
         System.out.println();
-        System.out.println("Would you like to check next city? YES: 1, NO: 0");
+        System.out.println("Would you like to check next city? YES: 1 or NO: 0");
         status = sc.nextInt();
     }
 }
